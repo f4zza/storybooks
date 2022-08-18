f2b1c58f4dd097363653fb4f976a88df5ec48a1a
f4zza <lazie.piscean@live.co.uk>
2022-08-18 23:24:41 +0530

    minor fixes

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -2,7 +2,7 @@ const moment = require('moment')
 
 module.exports = {
   formatDate: function (date, format) {
-    return moment(date).utc().format(format)
+    return moment(date).local().toDate() 
   },
   truncate: function (str, len) {
     if (str.length > len && str.length > 0) {
